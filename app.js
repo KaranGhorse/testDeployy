a9551305b957604377795fbe0f8a7fdd2c450f50
Karan Ghorse <118005333+KaranGhorse@users.noreply.github.com>
2024-04-24 12:53:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
 require('dotenv').config()
-const express = require('express')
-const app = express()
-const path = require('path')
+require('./DB/connection')
 
-app.get('/', (req,res)=>{
-    try {
-        res.sendFile(`${__dirname}/index.html`)
-    } catch (err)  {
-       res.send('something went wrong !') 
-    }
-})
-app.get('/about', (req,res)=>{
-    res.send('<h1>about page</h1>')
-})
+var express = require('express');
+var path = require('path');
+const cookieParser = require('cookie-parser')
+var indexRouter = require('./routes/index');
 
-app.get('/register', (req,res)=>{
-    res.send('<h1>register aala re</h1>')
-})
-app.get('/login', (req,res)=>{
-    res.send('<h1>login page</h1>')
+var app = express();
 
-})
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log(`app listining on 3000`);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+app.listen(3000, ()=>{
+    console.log('app listining on'+ 3000);
 })
